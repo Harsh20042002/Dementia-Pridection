f6cea4e984642f05fdd99b8155340c1bb0805186
Harsh20042002 <125402546+Harsh20042002@users.noreply.github.com>
2024-08-04 23:05:52 +0530

    Update About.js

--- Frontend/src/components/About.js
+++ Frontend/src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
 
   return (
     <div>
-      INotebook is a password-protected notebook app that utilizes full stack technology. It provides users with a secure and private space to store their notes, ideas, and thoughts without the fear of unauthorized access. The purpose of SecureNotebook is to ensure that the user’s personal and sensitive information remains confidential and protected from prying eyes. Whether you are a student, professional, or just someone who values privacy, SecureNotebook is the perfect tool.
+      Dementia is a password-protected prediction app that utilizes full-stack technology. It provides users with a secure and private space to store their X-ray reports and to predict the stage of dementia using that report without the fear of unauthorized access. The purpose of Dematia is to ensure that the user’s personal and sensitive information remains confidential and protected from prying eyes and they can predict the stage of their dementia without any error. Whether you are a doctor, professional, or just someone who is patient and values privacy, Dementia is the perfect tool.
     </div>
   )
 } 
